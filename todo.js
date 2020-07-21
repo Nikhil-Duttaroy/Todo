a5732cefd39e02324b714a3491342f8021b1c2a5
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2020-07-21 10:26:35 +0530

    Filter Added

--- todo.js
+++ todo.js
@@ -2,10 +2,12 @@
     const todoInput=document.querySelector(".todo-input")
     const todoButton=document.querySelector(".todo-Button")
     const todoList=document.querySelector(".todo-list")
-    const delTodo=document.querySelector(".trash-btn")
-    
+    const filterOption=document.querySelector(".filter-todo")
+    const todos=todoList.childNodes;
     todoButton.addEventListener('click',addTodo)
     // document.querySelector(".trash-btn").addEventListener('click',removeEventListener)
+    todoList.addEventListener('click',deleteCheck);
+    filterOption.addEventListener('click',filterTodo);
 
 
 function addTodo(event) {
@@ -35,4 +37,51 @@ function addTodo(event) {
 }
 
 
+function deleteCheck(event){
+    const item=event.target;
 
+    if(item.classList[0] === 'trash-btn'){
+        const todo=item.parentElement;
+        todo.classList.add("fall")
+        
+        todo.addEventListener('transitionend',function(){
+            todo.remove();
+        })
+    }
+
+    if(item.classList[0] ==='completed-btn'){
+        const todo=item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+
+function filterTodo(e){
+    //const todos=todoList.childNodes;
+    todos.forEach(function(todo){
+        if (todo.classList !== undefined) {
+            switch (e.target.value) {
+              case "all":
+                todo.style.display = "flex";
+                break;
+              case "completed":
+                if (todo.classList.contains("completed")) {
+                  todo.style.display = "flex";
+                } else {
+                  todo.style.display = "none";
+                }
+                break;
+                case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                  todo.style.display = "flex";
+                } else {
+                  todo.style.display = "none";
+                }
+                break;
+              default:
+                break;
+            }
+          }
+          return;
+        });
+}
